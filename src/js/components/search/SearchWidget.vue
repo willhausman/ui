8c1548da3cf360c7672d320ee33ff3f1188d7ca0
Will Hausman <will.jhausman@outlook.com>
2019-02-24 17:44:13 -0600

    search loads all the artifacts and heroes into memory

--- src/js/components/search/SearchWidget.vue
+++ src/js/components/search/SearchWidget.vue
@@ -1,7 +1,43 @@
 <template>
-    <input type="text" class="search" :value="searchText" />
+    <div class="search">
+        <input type="text" class="search-input" :value="searchText" />
+    </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+// import ResultsList from '@/js/components/search/ResultsList';
+
+export default {
+    data() {
+        return {
+            isLoading: false,
+        };
+    },
+    // components: {
+    //     ResultsList,
+    // },
+    computed: {
+        ...mapGetters('hero', { heroList: 'list' }), // this.heroList
+        ...mapGetters('artifact', { artifactList: 'list' }), // this.artifactList
+    },
+
+    mounted() {
+        const listsWaiting = [];
+
+        if (!this.heroList || !this.heroList.length) {
+            this.isLoading = true;
+            listsWaiting.push(this.$store.dispatch('hero/getList'));
+        }
+
+        if (!this.artifactList || !this.artifactList.length) {
+            this.isLoading = true;
+            listsWaiting.push(this.$store.dispatch('artifact/getList'));
+        }
+
+        Promise.all(listsWaiting).then(() => {
+            this.isLoading = false;
+        });
+    },
+};
 </script>
